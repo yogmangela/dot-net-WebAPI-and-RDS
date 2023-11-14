c0ed8924201d0c3d014a91aad55ca0505e9192da
yogmicroservices <yogmangela@hotmail.co.uk>
2023-11-14 14:53:36 +0000

    Commented out Pre-hooks

--- rds.tf
+++ rds.tf
@@ -3,21 +3,19 @@
 ################################################################################
 
 resource "aws_db_instance" "webapi" {
-  allocated_storage    = 10
-  db_name              = var.app_name
-  identifier           = "${var.app_name}-db"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "admin"
-  password             = aws_secretsmanager_secret_version.db-pass-val.secret_string
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  db_subnet_group_name = module.vpc.database_subnet_group
-  depends_on           = [module.vpc]
-  #   vpc_security_group_ids = [module.security_group.security_group_id]
+  allocated_storage      = 10
+  db_name                = var.app_name
+  identifier             = "${var.app_name}-db"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = aws_secretsmanager_secret_version.db-pass-val.secret_string
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = module.vpc.database_subnet_group
   vpc_security_group_ids = [module.security_group.security_group_id]
-  #["${var.vpc_security_group_ids}"]
+  depends_on             = [module.vpc]
 
   # multi_az = true # Enable RDS monitoring  
   # monitoring_interval = 5  # Enable RDS monitoring
