dabfe5b3c66f41521bb1bebf156e9c88c7553492
yogmicroservices <yogmangela@hotmail.co.uk>
2023-11-14 13:45:59 +0000

    Adding output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 
 # Output the RDS endpoint for reference
-# output "rds_endpoint" {
-#   value = aws_db_instance.webapi.endpoint
-# }
+output "rds_endpoint" {
+  value = aws_db_instance.webapi.endpoint
+}
 
-# output "webapi_endpoint" {
-#   value = aws_elastic_beanstalk_environment.webapi_env.endpoint_url
-# }
+output "webapi_endpoint" {
+  value = aws_elastic_beanstalk_environment.webapi_env.endpoint_url
+}
