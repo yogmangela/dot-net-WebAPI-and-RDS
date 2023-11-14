50d702a10f46621b59868d84886d74ecec8d9a2d
yogmicroservices <yogmangela@hotmail.co.uk>
2023-11-14 09:51:06 +0000

    Updated Readme.md

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 /*# Backend must remain commented until the Bucket
  and the DynamoDB table are created. 
  After the creation you can uncomment it,
- run "terraform init" and then "terraform apply" */
-
+ run "terraform init" and then "terraform apply" . */
 terraform {
   backend "s3" {
     bucket         = "infrastructure-terraform-state-backend"
